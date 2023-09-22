643fb9c64f3133d7abbcbee64acdd840b1bb7b12
Your Name <nguyenhoangthanh30703@gmail.com>
2023-09-22 08:49:44 +0700

    update

--- newyork/src/FetchData.js
+++ newyork/src/FetchData.js
@@ -3,29 +3,83 @@ import axios from "axios";
 
 function FetchData() {
   const [dataArray, setDataArray] = useState([]);
-  const [singleData, setSingleData] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
+  const [editData, setEditData] = useState(null);
 
   useEffect(() => {
     fetchDataArray();
-    fetchSingleData();
   }, []);
 
   const fetchDataArray = () => {
     axios
-      .get("https://yna1bjcoc3.execute-api.ap-southeast-1.amazonaws.com/v1/customers")
+      .get("http://175.41.185.23:8080/demo/api/v1/customers")
       .then((res) => {
         setDataArray(res.data);
       })
       .catch((err) => console.log(err));
   };
 
-  const fetchSingleData = () => {
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const addData = () => {
     axios
-      .get("https://yna1bjcoc3.execute-api.ap-southeast-1.amazonaws.com/v1/customers")
+      .post("http://175.41.185.23:8080/demo/api/v1/customers", formData)
       .then((res) => {
-        if (res.data.length > 0) { // Check if data is not empty
-          setSingleData(res.data[0]); // Set the first object in the response data
-        }
+        setDataArray([...dataArray, res.data]);
+        setFormData({
+          name: "",
+          email: "",
+          age: "",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const editDataFunc = (data) => {
+    setEditData(data);
+    setFormData({
+      name: data.name,
+      email: data.email,
+      age: data.age,
+    });
+  };
+
+  const updateData = () => {
+    axios
+      .put(`http://175.41.185.23:8080/demo/api/v1/customers/${editData.id}`, formData)
+      .then((res) => {
+        const updatedDataArray = dataArray.map((data) => {
+          if (data.id === res.data.id) {
+            return res.data;
+          }
+          return data;
+        });
+        setDataArray(updatedDataArray);
+        setEditData(null);
+        setFormData({
+          name: "",
+          email: "",
+          age: "",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteData = (id) => {
+    axios
+      .delete(`http://175.41.185.23:8080/demo/api/v1/customers/${id}`)
+      .then(() => {
+        const filteredDataArray = dataArray.filter((data) => data.id !== id);
+        setDataArray(filteredDataArray);
       })
       .catch((err) => console.log(err));
   };
@@ -33,7 +87,49 @@ function FetchData() {
   return (
     <div className="container">
       <div className="abc">
-        <h3>Fetch Data from API in React with Axios</h3>
+        <h3>Customer list</h3>
+        {/* Add data form */}
+        <form onSubmit={editData ? updateData : addData}>
+          <h4>{editData ? "Edit Data" : "Add Data"}</h4>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Age:</label>
+            <input
+              type="number"
+              name="age"
+              value={formData.age}
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            {editData ? "Update" : "Add"}
+          </button>
+          {editData && (
+            <button type="button" className="btn btn-secondary ml-2" onClick={() => setEditData(null)}>
+              Cancel
+            </button>
+          )}
+        </form>
         {/* Rendering the array of objects */}
         <table className="table">
           <thead>
@@ -42,6 +138,7 @@ function FetchData() {
               <th>Name</th>
               <th>Email</th>
               <th>Age</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -51,24 +148,21 @@ function FetchData() {
                 <td>{data.name}</td>
                 <td>{data.email}</td>
                 <td>{data.age}</td>
+                <td>
+                  <button type="button" className="btn btn-primary mr-2" onClick={() => editDataFunc(data)}>
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger" onClick={() => deleteData(data.id)}>
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
-
-        {/* Rendering the single object */}
-        {singleData && (
-          <div>
-            <h4>Single Object:</h4>
-            <p>ID: {singleData.id}</p>
-            <p>Name: {singleData.name}</p>
-            <p>Email: {singleData.email}</p>
-            <p>Age: {singleData.age}</p>
-          </div>
-        )}
       </div>
     </div>
   );
 }
 
-export default FetchData;
+export default FetchData; 
